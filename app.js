10c1ad907d2840c3c48ff1f7440d6bf086e8cdf3
jjwyse <josh@cloud-elements.com>
2014-07-26 19:01:04 -0600

    Handle PUT too

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ if ('development' == app.get('env')) { app.use(express.errorHandler()); }
 
 app.get('/', routes.index);
 app.post('/', routes.callback);
+app.put('/', routes.callback);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
